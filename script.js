db943771d56014b89cb602c7325adcad049e9107
unknown <pural.johnashleyc@gmail.com>
2024-03-14 15:50:55 +0800

    latest updates

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
+/* Fonts and Icons */
+    WebFont.load({
+		google: {"families":["Open+Sans:300,400,600,700"]},
+		custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands"], urls: ['assets/css/fonts.css']},
+		active: function() {
+			sessionStorage.fonts = true;
+		}
+	});
+
+
 const sidebarToggle = document.querySelector("#sidebar-toggle");
 sidebarToggle.addEventListener("click", function(){
     document.querySelector("#sidebar").classList.toggle("collapsed")
 });
+
+document.querySelector(".theme-toggle").addEventListener("click",() =>{
+    toggleLocalStorage();
+    toggleRootClass();
+
+});
+
+function toggleRootClass(){
+    const current = document.documentElement.getAttribute('data-bs-theme');
+    const inverted = current == 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-bs-theme', inverted);
+}
+
+function toggleLocalStorage(){
+    if(isLight()){
+        localStorage.removeItem("light");
+    }else{
+        localStorage.setItem("light","set");
+    }
+}
+
+function isLight(){
+    return localStorage.getItem("light");
+}
+
+if (isLight()){
+    toggleRootClass();
+}
